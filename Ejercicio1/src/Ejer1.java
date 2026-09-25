a4113684fe8875fda9631589909fba28684d3ce4
jdelvalle8 <jdelvalle@ikaslea.birt.eus>
2026-09-25 13:30:27 +0200

    Add files via upload

---
+++ Ejercicio1/src/Ejer1.java
@@ -0,0 +1,34 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Ejer1 {
+    public static void main(String[] args) {    	
+        // Usamos try-catch para manejar las excepciones
+        try (FileReader reader = new FileReader("entrada.txt");
+            FileWriter writer = new FileWriter("salida.txt")) {        	
+            int dato;
+            // Mientras pueda leer seguirá haciéndolo
+            while ((dato = reader.read()) != -1) {                
+                // Convertimos el int a char para poder transformarlo
+                char caracter = (char) dato;                
+                // Aplicamos las transformaciones
+                if (Character.isDigit(caracter)) {
+                    // Si es un número escribimos un #
+                    writer.write('#');
+                } else if (Character.isLetter(caracter)) {
+                    // Si es una letra la pasamos a mayúscula
+                    writer.write(Character.toUpperCase(caracter));
+                } else {
+                	// El resto los mantenemos igual
+                    writer.write(caracter);
+                }
+            }            
+            System.out.println("Proceso terminado.");
+            
+        } catch (IOException e) {
+            // Capturamos el error si falla la entrada o salida
+            System.err.println("Error de entrada/salida: " + e.getMessage());
+        }
+    }
+}
